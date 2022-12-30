10c2d708edf0d19f0dd7cf90ae62598f9434f600
Amina <aminarahal39@gmail.com>
2022-12-30 18:11:14 +0100

    Added the player radius

--- Assets/Amina/scripts/Player.cs
+++ Assets/Amina/scripts/Player.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public LineRenderer circleRenderer;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        DrawCircle(50,1);
     }
 
-    // Update is called once per frame
-    void Update()
+    void DrawCircle(int steps, float radius) 
     {
-        
+        circleRenderer.positionCount = steps;
+        for(int currentStep = 0; currentStep < steps; currentStep++)
+        {
+            float circumferenceProgress = (float)currentStep/steps;
+            
+            float currentRadian = circumferenceProgress * 3 * Mathf.PI;
+
+            float xScaled = Mathf.Cos(currentRadian);
+            float yScaled = Mathf.Sin(currentRadian);
+
+            float x = xScaled * radius;
+            float y = yScaled * radius;
+
+            Vector3 currentPosition = new Vector3(x, y, 0);
+
+            circleRenderer.SetPosition(currentStep, currentPosition);
+        }
     }
 }
